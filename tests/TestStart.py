5eb88848448d726d4e320cd0adb1679d6cee36aa
Erica M. ("Loonatic") <l.oony@aol.com>
2023-01-15 21:46:38 -0600

    Update TestStart.py

--- tests/TestStart.py
+++ tests/TestStart.py
@@ -1,17 +1,9 @@
-from modtools.toontown.toonbase import ModularStart
-from modtools.toontown.toonbase.ModularBase import ModularBase
+from modtools.modbase import ModularStart
+from modtools.modbase.ModularBase import ModularBase
 
 base = ModularBase()
 base.initCR()
 
-base.generateLocalAvatar()
-base.localAvatar.reparentTo(render)
-base.localAvatar.hp = 15  # hack
-testModel = loader.loadModel("phase_4/models/neighborhoods/toontown_central")
-testModel.reparentTo(render)
-
-# base.modular = False
 # base.startConnection()
 
-base.oobe()
 base.run()
